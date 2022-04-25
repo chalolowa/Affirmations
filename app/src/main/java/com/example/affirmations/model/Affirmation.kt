db4f195b29811b52531b6cd9b57f2827c777c166
locha <wangwelocha@students.uonbi.ac.ke>
2022-04-25 13:56:50 +0300

    final commit

---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,7 @@
+package com.example.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation (@StringRes val StringResourceId: Int, @DrawableRes val ImageResourceId: Int) {
+}
